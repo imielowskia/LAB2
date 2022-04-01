564a8db6e10da44b72ae836e9b71bf1cca3394a4
imielowskia <andrzej.imielowski@pwste.edu.pl>
2022-04-01 17:45:52 +0200

    pierwszy commit - aplikacja w wersji podstawowej

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :students
+  resources :groups
+  resources :courses
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
